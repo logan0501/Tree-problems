661069c9ae9f6d0b930bfc787aa69eb4fdf2396b
Loganathan <loganvk18@gmail.com>
2021-11-29 11:37:03 +0530

    initial commit

---
+++ postOrderTraversal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    vector<int> v;
+    void post(TreeNode* root){
+        if(root){
+            post(root->left);
+            post(root->right);
+            v.push_back(root->val);
+        }
+    }
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        post(root);
+        return v;
+    }
+};
+int main()
+{
+
+    return 0;
+}
---
+++ preordertraversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    vector<int> v;
+    void pre(TreeNode *root)
+    {
+        if (root)
+        {
+            v.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+    }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+
+        pre(root);
+        return v;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
