9e324d28d0806d9d29cf90dd1e90e36a5962ed61
Loganathan <loganvk18@gmail.com>
2021-12-09 19:21:16 +0530

    solved leetcode problems

---
+++ symmetricTree.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Link - https://leetcode.com/problems/symmetric-tree/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isSymmetric(TreeNode* root) {
+    if(!root)return true;
+    if(!root->left && )
+}
+int main(){
+
+    return 0;
+}
